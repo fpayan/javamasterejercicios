20fcb77cdf49266a4c0a459a12ddb9b36f4f5632
Francisco Payan Calero <fpayan.calero@gmail.com>
2019-03-05 21:47:14 +0100

    first commit and first exercise of master java full stack.

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,40 @@
+package holamundo;
+
+/**
+ * Clase de ejemplo para mostrar los argumentos pasados por linea de comando 
+ * cuando ejecutamos esta clase con varios argumentos. Ejem: java HolaMundo Arg1 Arg2 etc..
+ * 
+ * @author fpayan
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Saludamos
+        System.out.println("Hola " + saludoPorArgumnentosMain(args));
+    }
+    
+    /**
+     * Construye la cadena a devolver bien formada si el array que se le pasa como parámetro tiene un tamaño superior a cero.
+     * 
+     * @param Array<String> array de String a concatenar.
+     * 
+     * @return String cadena pasada por parámetro
+     */
+    static String saludoPorArgumnentosMain(String[] _args){
+        StringBuilder saludo = new StringBuilder();
+        // More arguments
+        if(_args.length > 0){
+            // Yes arguments
+            for(String data: _args){
+                saludo.append(data).append(" ");
+            }
+        }else{
+            // Not arguments
+            saludo.append("Invitado");
+        }     
+        return saludo.toString();
+    }
+}
